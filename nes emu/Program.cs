a84cd19613b6061f638bf0b0826ee173436bb15d
Anumania <jonahmcathcart@gmail.com>
2021-10-14 16:11:00 -0700

    Add project files.

---
+++ nes emu/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace nes_emu
+{
+    class Program { 
+
+        static void Main(string[] args)
+        {
+            
+        }
+
+        static void Parse()
+        {
+            
+        }
+    }
+
+    class Instructions
+    {
+        void x00() //BRK
+        {
+            Console.WriteLine("brk"); //(bork)
+        }
+        
+        void x01() //ORA Indirect, X
+        {
+
+        }
+
+    }
+
+    static class Registers
+    {
+        static byte accumulator;
+        static byte indexX;
+        static byte indexY;
+        static bool[] statusFlags = new bool[7];
+        static byte stackPTR;
+        static ushort PC;
+    }
+}
+
